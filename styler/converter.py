5243e48767b2965fa68a0e686a84b202238b488b
Mat Munn <mat@savi.com.au>
2016-11-29 10:43:59 +1100

    Removed pointer-events: none from phone number links
    Signed-off-by: Mat Munn <mat@savi.com.au>

--- styler/converter.py
+++ styler/converter.py
@@ -27,7 +27,7 @@ class Conversion:
 		aggregateCSS="";
 		if len(srcPrefix) and not srcPrefix.endswith('/'):
 			srcPrefix = srcPrefix + '/'
-			
+
 		# retrieve CSS rel links from html pasted and aggregate into one string
 		CSSRelSelector = CSSSelector("link[rel=stylesheet],link[rel=StyleSheet],link[rel=STYLESHEET]")
 		matching = CSSRelSelector.evaluate(document)
@@ -43,7 +43,7 @@ class Conversion:
 				element.getparent().remove(element)
 			except:
 				raise IOError('The stylesheet '+element.get("href")+' could not be found')
-		
+
 		#include inline style elements
 		print aggregateCSS
 		CSSStyleSelector = CSSSelector("style,Style")
@@ -51,17 +51,17 @@ class Conversion:
 		for element in matching:
 			aggregateCSS+=element.text
 			element.getparent().remove(element)
-		
+
 		#convert  document to a style dictionary compatible with etree
 		styledict = self.getView(document, aggregateCSS)
-		
+
 		#set inline style attribute if not one of the elements not worth styling
 		ignoreList=['html','head','title','meta','link','script','unsubscribe','repeater','singleline','multiline','br','layout']
 		for element, style in styledict.items():
 			if element.tag not in ignoreList:
 				v = style.getCssText(separator=u'')
 				element.set('style', v)
-		
+
 		#convert tree back to plain text html
 		self.convertedHTML = etree.tostring(document, method="xml", pretty_print=True,encoding='UTF-8')
 		self.convertedHTML= self.convertedHTML.replace('&#13;', '') #tedious raw conversion of line breaks.
@@ -91,16 +91,15 @@ class Conversion:
 			soup.html.head.append(soup.new_tag('style', type="text/css"))
 		soup.html.head.style.append("""
 		a[href^="x-apple-data-detectors:"] {
-		    color: #fff;
+		    color: #000000;
 		    text-decoration: none;
 		}
       	a[href^="tel"], a[href^="sms"] {
 	        text-decoration: none;
 	        color: #000000;
-	        pointer-events: none;
         }
 		""")
-		
+
 		for img in soup.find_all('img'):
 			if 'spacer.gif' in img.get('src'):
 				classes = img.get('class')
@@ -111,9 +110,9 @@ class Conversion:
 						img.parent['height'] = img.get('height')
 
 		self.convertedHTML = str(soup)
-		
+
 		return self
-		
+
 	def styleattribute(self,element):
 		"""
 		returns css.CSSStyleDeclaration of inline styles, for html: @style
@@ -123,44 +122,44 @@ class Conversion:
 			return cssutils.css.CSSStyleDeclaration(cssText=cssText)
 		else:
 			return None
-			
+
 	def getView(self, document, css):
-		
+
 		view = {}
 		specificities = {}
 		supportratios={}
 		supportFailRate=0
 		supportTotalRate=0;
 		compliance=dict()
-		
+
 		#load CSV containing css property client support into dict
 		mycsv = csv.DictReader(open(os.path.join(settings.FILEROOT, "css_compliance.csv")), delimiter=',')
 		for row in mycsv:
 			#count clients so we can calculate an overall support percentage later
 			clientCount=len(row)
 			compliance[row['property'].strip()]=dict(row);
-		
+
 		#decrement client count to account for first col which is property name
 		clientCount-=1
-		
+
 		#sheet = csscombine(path="http://www.torchbox.com/css/front/import.css")
 		sheet = cssutils.parseString(css)
-		
-		rules = (rule for rule in sheet if rule.type == rule.STYLE_RULE)    
+
+		rules = (rule for rule in sheet if rule.type == rule.STYLE_RULE)
 		for rule in rules:
-			
+
 			for selector in rule.selectorList:
 				try:
 					cssselector = CSSSelector(selector.selectorText)
 					matching = cssselector.evaluate(document)
-					
+
 					for element in matching:
 						# add styles for all matching DOM elements
 						if element not in view:
 							# add initial
 							view[element] = cssutils.css.CSSStyleDeclaration()
 							specificities[element] = {}
-							
+
 							# add inline style if present
 							inlinestyletext= element.get('style')
 							if inlinestyletext:
@@ -172,14 +171,14 @@ class Conversion:
 									# set inline style specificity
 									view[element].setProperty(p)
 									specificities[element][p.name] = (1,0,0,0)
-								
+
 						for p in rule.style:
 							#create supportratio dic item for this property
 							if p.name not in supportratios:
 								supportratios[p.name]={'usage':0,'failedClients':0}
 							#increment usage
 							supportratios[p.name]['usage']+=1
-							
+
 							try:
 								if not p.name in self.CSSUnsupportErrors:
 									for client, support in compliance[p.name].items():
@@ -196,10 +195,10 @@ class Conversion:
 													self.CSSUnsupportErrors[p.name].append(client + ' (partial support)')
 												else:
 													self.CSSUnsupportErrors[p.name].append(client)
-											
+
 							except KeyError:
 								pass
-													
+
 							# update styles
 							if p not in view[element]:
 								view[element].setProperty(p.name, p.value, p.priority)
@@ -207,18 +206,18 @@ class Conversion:
 							else:
 								sameprio = (p.priority == view[element].getPropertyPriority(p.name))
 								if not sameprio and bool(p.priority) or (sameprio and selector.specificity >= specificities[element][p.name]):
-									# later, more specific or higher prio 
+									# later, more specific or higher prio
 									view[element].setProperty(p.name, p.value, p.priority)
-									
+
 				except ExpressionError:
 					if str(sys.exc_info()[1]) not in self.CSSErrors:
 						self.CSSErrors.append(str(sys.exc_info()[1]))
 					pass
-		
+
 		for props, propvals in supportratios.items():
 			supportFailRate+=(propvals['usage']) * int(propvals['failedClients'])
 			supportTotalRate+=int(propvals['usage']) * clientCount
-		
+
 		if(supportFailRate and supportTotalRate):
 			self.supportPercentage= 100- ((float(supportFailRate)/float(supportTotalRate)) * 100)
 		return view
